3028131dffc988bb873e2756732c2e257576c86e
tameni <jsilva@uni.pe>
2023-03-30 23:24:16 -0500

    Add theme :nail_care:

--- init.lua
+++ init.lua
@@ -85,16 +85,16 @@ require('lazy').setup({
       },
     },
   },
-
   {
-    -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme 'onedark'
-    end,
+    'rose-pine/neovim',
+    name='rose-pine',
+    config= function()
+      require('rose-pine').setup({
+        disable_italics=true
+      })
+      vim.cmd('colorscheme rose-pine')
+    end
   },
-
   {
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
